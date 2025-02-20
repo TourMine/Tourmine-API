238e86e8e8204f6cfe1b9d8ed21f0774cea27980
ClodsNegreiros <clodsn@gmail.com>
2025-02-20 03:26:45 -0300

    feat: implementado integração com api de e-mail

---
+++ src/Tourmine.Application/Requests/Email/SendEmailRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace Tourmine.Application.Requests.Email
+{
+    public class SendEmailRequest
+    {
+        public EmailSender From { get; set; }
+        public List<EmailRecipient> To { get; set; }
+        public string Subject { get; set; }
+        public string template_id { get; set; } // Corrigido para 'template_id'
+        public List<Personalization> Personalization { get; set; }
+    }
+
+    public class EmailSender
+    {
+        public string Email { get; set; }
+    }
+
+    public class EmailRecipient
+    {
+        public string Email { get; set; }
+    }
+
+    public class Personalization
+    {
+        public string Email { get; set; }
+        public Dictionary<string, string> Data { get; set; }
+    }
+}
