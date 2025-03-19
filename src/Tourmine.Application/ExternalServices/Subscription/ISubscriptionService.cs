cbf97f232b62e54140ad22c135b7ab36e5280b4d
ClodsNegreiros <clodsn@gmail.com>
2025-03-19 19:47:57 -0300

    feat: implementado integração entre módulo de subscription e bff

---
+++ src/Tourmine.Application/ExternalServices/Subscription/ISubscriptionService.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using Tourmine.Application.Requests.Subscription;
+
+namespace Tourmine.Application.ExternalServices.Subscription
+{
+    public interface ISubscriptionService
+    {
+        [Post("/subscription/v1/create")]
+        Task<HttpResponseMessage> Create([Body] CreateSubscriptionRequest request);
+
+        [Get("/subscription/v1/{UserId}")]
+        Task<HttpResponseMessage> GetAllByUserId(
+            Guid id,
+            [Query] int start = 0,
+            [Query] int limit = 25
+            );
+
+        [Put("/subscription/v1/{UserId}/{TournamentId}")]
+        Task<HttpResponseMessage> Update(
+            Guid UserId,
+            Guid TournamentId,
+            [Body] UpdateSubscriptionRequest request);
+
+        [Get("/subscription/v1/get-by-tournamentId/{tournamentId}")]
+        Task<HttpResponseMessage> GetAll(
+            Guid TournamentId,
+            [Query] GetAllSubscriptionByTournamentIdRequest request);
+    }
+}
---
+++ src/Tourmine.Application/Requests/Subscription/UpdateSubscriptionRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Tourmine.Application.Requests.Subscription
+{
+    public class UpdateSubscriptionRequest
+    {
+        public ESubscriptionStatus Status { get; set; }
+    }
+
+    public enum ESubscriptionStatus
+    {
+        ACTIVE = 1,
+        DESACTIVE = 2
+    }
+}
