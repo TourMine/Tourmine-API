727c80c5330fab23089351a6e8e92d6d944af7dd
ClodsNegreiros <clodsn@gmail.com>
2025-03-19 19:48:08 -0300

    feat: implementado integração entre módulo de tournament e bff

---
+++ src/Tourmine.Application/ExternalServices/Tournament/ITournamentService.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using Tourmine.Application.Requests.Tournament;
+
+namespace Tourmine.Application.ExternalServices.Tournament
+{
+    public interface ITournamentService
+    {
+        [Post("/tournament/v1/create")]
+        Task<HttpResponseMessage> Register([Body] CreateTournamentRequest request);
+
+        [Get("/tournament/v1/{id}")]
+        Task<HttpResponseMessage> GetById([Query] Guid id);
+
+        [Put("/tournament/v1/{id}")]
+        Task<HttpResponseMessage> Update(Guid id, [Body] UpdateTournamentRequest request);
+
+        [Get("/tournament/v1/all")]
+        Task<HttpResponseMessage> GetAll([Query] GetTournamentAllRequest request, int start = 0, int limit = 25);
+    }
+}
---
+++ src/Tourmine.Application/Requests/Tournament/GetTournamentAllRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Tourmine.Application.Requests.Tournament
+{
+    public class GetTournamentAllRequest
+    {
+        public EPlataforms? plataforms { get; set; }
+        public EParticipantsType? teamsTypes { get; set; }
+        public ESubscriptionType? subscriptionTypes { get; set; }
+    }
+}
